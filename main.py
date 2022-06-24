a6e52f1e5b07bf7f6239c7f5ab6d3b6b8d724527
HonzaKubita <honza.kubita@gmail.com>
2022-06-24 10:47:34 +0200

    Added modules

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from argparser import argParser# Library for parsing shell parameters
 
+# Modules import
+import compiler
+import builder
+import runner
+
 parserTemplate = ['mode', 'fileName'] # Create template of shell arguments
 parser = argParser(parserTemplate)
 
