68aab6341afbb8c22890b055ca6f673f795698fd
HonzaKubita <honza.kubita@gmail.com>
2022-07-09 00:15:29 +0200

    Addded description to loading

--- loading.py
+++ loading.py
@@ -8,13 +8,14 @@ class Loading:
     self.l_border = border[len(border)//2:]
     self.fill = fill
     self.empty = empty
+    self.description = ""
 
   def _render(self):
     # capacity 10
     # filled 5
     percentage = self.filled / self.capacity
     percentageFill = m.floor(percentage * self.size)
-    print(f'{ self.r_border }{ self.fill * percentageFill }{ self.empty * ( self.size - percentageFill ) }{ self.l_border } {m.floor(percentage * 100)}%', end = '\r' if self.filled < self.capacity else '\n')
+    print(f'{ self.r_border }{ self.fill * percentageFill }{ self.empty * ( self.size - percentageFill ) }{ self.l_border } {m.floor(percentage * 100)}% {self.description}', end = '\r' if self.filled < self.capacity else '\n')
 
   def start(self):
     self._render()
@@ -25,4 +26,8 @@ class Loading:
 
   def remove(self, amount):
     self.filled -= amount
+    self._render()
+
+  def addDescription(self, description):
+    self.description = description
     self._render()
